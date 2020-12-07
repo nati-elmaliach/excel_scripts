ca8d161db805527d140fdc17e872c7d904f8bfcd
nati-elmaliach <natielmaliach3197@gmail.com>
2020-12-07 23:48:32 +0200

    removing duplicate code. version 0.1.0 first version magor minor patch

--- script.py
+++ script.py
@@ -3,82 +3,32 @@ from os.path import isfile, join
 import pandas as pd
 import math
 
-# Todo ask if thoe columns are static
 columns_map = {
     "pass": 0,
     "per_year_per_pair": 21,
     "avg_winr": 22,
     "avg_dd": 23,
-    "high_dd": 24
+    "high_dd": 24,
+    "std": 25
 }
 
-valid_passes = []
+
+def print_row(*args):
+    print("-----------------")
+    print(args)
+    print("-----------------")
 
 
-# first row column number 21
-# per_year_per_pair = data.iloc[0].iloc[21]
-# print(per_year_per_pair)
-# avg_winr = data.iloc[1].iloc[22]
-
-# print(per_year_per_pair >=55)
-# print(avg_winr)
-
-
-def get_column_value(index, col_name, data , should_print = False):
+def get_column_value(index, col_name, data, should_print=False):
     if should_print:
         print(index)
         print(col_name)
 
-    return data.iloc[index].iloc[columns_map[col_name]]
+    return data.iloc[index - 1].iloc[columns_map[col_name]]
 
 
-def compare_passes(file_list, valid_passes):
-    count = 0
-    filterd_rows = []
-
-    for file_name in file_list:
-        #print(
-         #   f"========================================Checking file  name {file_name}")
-        data = get_stdo_data(file_name)[0]
-        iter_over = valid_passes if count == 0 else filterd_rows
-
-        for pass_number in iter_over:
-            index = pass_number - 1
-
-            per_year_per_pair = get_column_value(
-                index,  "per_year_per_pair", data)
-            avg_winr = get_column_value(index,  "avg_winr", data)
-            avg_dd = get_column_value(index,  "avg_dd", data) * 100
-            high_dd = get_column_value(index,  "high_dd", data) * 100
-
-            # print("-----------------")
-
-            # print(per_year_per_pair)
-
-            # print(avg_winr)
-
-            # print(avg_dd)
-
-            # print(high_dd)
-            # print("-----------------")
-
-            if validate_row_at_index(per_year_per_pair, avg_winr, avg_dd, high_dd) and count == 0:
-                filterd_rows.append(pass_number)
-            elif count > 0 and not validate_row_at_index(per_year_per_pair, avg_winr, avg_dd, high_dd):
-                try:
-                    filterd_rows.pop(iter_over.index(pass_number))
-                except:
-                    print(f"No match in pass: {pass_number}")
-
-        count += 1
-        print(filterd_rows)
-
-
-    print(filterd_rows)
-
-
-def validate_row_at_index(per_year_per_pair, avg_winr, avg_dd, high_dd):
-    isTrue = True if per_year_per_pair >= 0 and avg_winr >= 69.99 and avg_dd >= 0 and high_dd >= 0 else False
+def validate_row_at_index(per_year_per_pair, avg_winr, avg_dd, high_dd , std):
+    isTrue = True if per_year_per_pair >= 0 and avg_winr >= 69.99 and avg_dd >= 0 and high_dd >= 0 and std > 4999 else False
     if isTrue:
         print(True)
     return isTrue
@@ -86,45 +36,26 @@ def validate_row_at_index(per_year_per_pair, avg_winr, avg_dd, high_dd):
 # 5
 
 
-def get_valid_rows(data, ending_row_index, ex_file):
-    vliad_passes = []
-    for index in range(ending_row_index):
+def get_valid_rows(data, ex_file, valid_rows_pass):
+    filterd_rows = []
+    for index in valid_rows_pass:
         per_year_per_pair = get_column_value(index,  "per_year_per_pair", data)
         avg_winr = get_column_value(index,  "avg_winr", data)
         avg_dd = get_column_value(index,  "avg_dd", data) * 100
         high_dd = get_column_value(index,  "high_dd", data) * 100
+        std = get_column_value(index,  "std", data)
 
-        # print("-----------------")
+        #print_row(per_year_per_pair, avg_winr, avg_dd, high_dd)
 
-        # print(per_year_per_pair)
-
-        # print(avg_winr)
-
-        # print(avg_dd)
-
-        # print(high_dd)
-        # print("-----------------")
-
-        # if per_year_per_pair > 370:
-        #   print(f"per_year_per_pair {per_year_per_pair}")
-        #  if avg_winr >= 0:
-        #     print("avg_winr")
-        #    if avg_dd >=0:
-        #       print("avg_dd")
-        #      if high_dd >=0 :
-        #         print("high_dd")
-        #        print(index)
-
-        if validate_row_at_index(per_year_per_pair, avg_winr, avg_dd, high_dd):
+        if validate_row_at_index(per_year_per_pair, avg_winr, avg_dd, high_dd , std):
             one_pass = get_column_value(index,  "pass", data)
-            valid_passes.append(one_pass)
+            filterd_rows.append(one_pass)
 
-    return valid_passes
+    return filterd_rows
 
 
 # 4
 def get_file_ending_row_index(data, ex_file):
-    
 
     ending_row_index = data.shape[0]
     for row_index in range(ending_row_index):
@@ -162,12 +93,20 @@ def get_stdo_data(file_name):
 def start(excel_files):
     one_excel_file = excel_files.pop()
     data, ending_row_index = get_stdo_data(one_excel_file)
-    valid_rows_pass = get_valid_rows(data, ending_row_index, one_excel_file)
+    valid_rows_pass = get_valid_rows(
+        data, one_excel_file, [x for x in range(ending_row_index)])
 
     print(valid_rows_pass)
 
     if len(excel_files) > 0:
-        compare_passes(excel_files, valid_rows_pass)
+        for file_name in excel_files:
+            data = get_stdo_data(file_name)[0]
+
+            valid_rows_pass = get_valid_rows(
+                data,  file_name, valid_rows_pass)
+
+            print(valid_rows_pass)
+
 
 # 1
 
